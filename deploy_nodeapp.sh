1089567f74bba57706f6bdca02d5d19b9c8f71dd
WorWh3r3 <worzwhere@gmail.com>
2020-03-28 22:46:02 +0700

    Update deploy_nodeapp.sh

--- deploy_nodeapp.sh
+++ deploy_nodeapp.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-git clone https://github.com/worzwhere/nodejsapp.git nodeapp
 cd nodeapp
 git clone https://github.com/CleverCloud/demo-nodejs-mongodb-rest.git app
 docker-compose build
